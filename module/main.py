00d03bf40c8c36badca608c1d9cea51c3630bb37
IDNGamer <idngamerofficial@gmail.com>
2025-08-22 20:23:31 +0700

    Add files via upload

---
+++ module/main.py
@@ -0,0 +1,14 @@
+import mymodule as mm
+
+
+area1 = mm.area_of_square(6)
+print(f"Area of square with side = 6 is {area1}")
+
+area1 = mm.area_of_square(12)
+print(f"Area of square with side = 12 is {area1}")
+
+area1= mm.area_of_triangle(4,5)
+print(f"Area of triangle with base = 4, height = 5 is {area1}")
+
+area1 = mm.area_of_circle(7)
+print(f"area of circle with radius = 7 is {area1}")
---
+++ module/mymodule.py
@@ -0,0 +1,12 @@
+import math
+
+def area_of_square(s):
+    area = s * s
+    return area
+
+def area_of_triangle(base, height):
+    area = 0.5 * base * height
+    return area
+
+def area_of_circle(r):
+    area = 3.14 * r * r
